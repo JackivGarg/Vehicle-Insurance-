eb498f880fca043daf0937e32270eadf610a4ce7
JackivGarg <xplorerjackiv11@gmail.com>
2026-03-20 11:39:56 +0530

    data ingestion

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
-from src.logger import logging
+# from src.logger import logging
 
-logging.debug("This is a debug message.")
+# logging.debug("This is a debug message.")
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
